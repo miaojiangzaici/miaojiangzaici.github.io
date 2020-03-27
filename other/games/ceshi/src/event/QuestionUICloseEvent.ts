17cc18768ae8f2dcb5fbdea3d00b889c40faa087
miaojiangzaici <862791687@qq.com>
2020-03-27 17:55:33 +0800

    Site updated: 2020-03-27 17:55:13

--- other/games/ceshi/src/event/QuestionUICloseEvent.ts
+++
@@ -1,8 +0,0 @@
-class QuestionUICloseEvent extends egret.Event {
-    public static NAME: string = "QuestionUICloseEvent";
-    public num: number;
-
-    public constructor(type: string, bubbles: boolean = false, cancelable: boolean = false) {
-        super(type, bubbles, cancelable);
-    }
-}
--- other/games/ceshi/src/event/TabFireEvent.ts
+++
@@ -1,8 +0,0 @@
-class TabFireEvent extends egret.Event
-{
-    public static NAME:string = "TabFireEvent";
-    public constructor(type:string, bubbles:boolean=false, cancelable:boolean=false)
-    {
-        super(type,bubbles,cancelable);
-    }
-}
